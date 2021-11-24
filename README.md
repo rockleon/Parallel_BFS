fc9de965f4b0d56aa3ad73252f3431456a0a4e58
rockleon <ameyphantom07@gmail.com>
2021-11-24 12:42:18 +0530

    parallel bfs code

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Parallel_BFS
 Implement BFS using parallel algorithms strategy and compare it with serial bfs
+
+
+## Description of files
+
+### generate_graph.cpp
+Creates a 2-D matrix representation for a dummy graph in "dummy.txt" file.
+Matrix consists of "0's" and "1's".
+
+### bfs.cpp
+Main function of both Serial and Parallel BFS code as well as the interface code.
+
+
+## Steps for execution
+
+```sh
+g++ -o a.out -fopenmp bfs.cpp
+./a.out
+```
--- bfs.cpp
+++ bfs.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include <list>
+#include <vector>
 #include <sstream>
 #include <string>
 #include <fstream>
 #include <time.h>
 #include <iomanip>
+#include <stdio.h>
+#include <omp.h>
 
 using namespace std;
 
@@ -21,14 +24,18 @@ public:
     // Add an edge to graph
     void addEdge(int v, int w);
 
-    // BFS traversal from a given source s
-    void BFS(int s);
-
     // Create graph using user entered data
     int inputGraph();
 
     // Generate graph using data from dummy file
     int generateDummyGraph();
+
+    // BFS traversal from a given source s
+    void BFS(int s);
+
+    // Parallel BFS traversal from given source s
+    // using openmp
+    void parallel_BFS(int s);
 };
 
 Graph::Graph(int V)
@@ -40,41 +47,6 @@ Graph::Graph(int V)
 void Graph::addEdge(int v, int w)
 {
     adj[v].push_back(w);
-}
-
-void Graph::BFS(int s)
-{
-    // Mark all the vertices as not visited
-    bool *visited = new bool[V];
-    for (int i = 0; i < V; i++)
-        visited[i] = false;
-
-    // Queue for BFS
-    list<int> queue;
-
-    // Mark the current node as visited and enqueue it
-    visited[s] = true;
-    queue.push_back(s);
-
-    while (!queue.empty())
-    {
-        // Dequeue a vertex from queue and print it
-        s = queue.front();
-        // cout << s << " ";
-        queue.pop_front();
-
-        // Get all adjacent vertices of the dequeued
-        // vertex s. If a adjacent has not been visited,
-        // then mark it visited and enqueue it
-        for (auto i = adj[s].begin(); i != adj[s].end(); ++i)
-        {
-            if (!visited[*i])
-            {
-                visited[*i] = true;
-                queue.push_back(*i);
-            }
-        }
-    }
 }
 
 int Graph::inputGraph()
@@ -128,6 +100,92 @@ int Graph::generateDummyGraph()
     return edges;
 }
 
+void Graph::BFS(int s)
+{
+    // Mark all the vertices as not visited
+    bool *visited = new bool[V];
+    for (int i = 0; i < V; i++)
+        visited[i] = false;
+
+    // Queue for BFS
+    list<int> queue;
+
+    // Mark the current node as visited and enqueue it
+    visited[s] = true;
+    queue.push_back(s);
+
+    while (!queue.empty())
+    {
+        // Dequeue a vertex from queue and print it
+        s = queue.front();
+        // cout << s << " ";
+        queue.pop_front();
+
+        // Get all adjacent vertices of the dequeued
+        // vertex s. If a adjacent has not been visited,
+        // then mark it visited and enqueue it
+        for (auto i = adj[s].begin(); i != adj[s].end(); ++i)
+        {
+            if (!visited[*i])
+            {
+                visited[*i] = true;
+                queue.push_back(*i);
+            }
+        }
+    }
+}
+
+void Graph::parallel_BFS(int s)
+{
+    // Mark all the vertices as not visited
+    bool *visited = new bool[V];
+    for (int i = 0; i < V; i++)
+        visited[i] = false;
+
+    // Two Queues for parallel BFS
+    // One queue to loop through pushed vertices
+    // Another queue to push vertices for next iteration.
+    vector<vector<int>> queue = {{}, {}};
+    int turn = 0, next_turn;
+
+    // Mark the current node as visited and enqueue it
+    visited[s] = true;
+    queue[turn].push_back(s);
+
+    while (!queue[turn].empty())
+    {
+        // get other queue's index
+        next_turn = (turn + 1) % 2;
+
+        // Dequeue a vertex from queue and print it
+        // Here we use openmp parallel threading
+        // Whatever the number of vertices present in the queue,
+        // That many threads will be created and executed parallely
+        #pragma omp parallel for
+        for (int j = 0; j < queue[turn].size(); j++)
+        {
+            s = queue[turn][j];
+            // cout << s << " ";
+
+            // Get all adjacent vertices of the dequeued
+            // vertex s. If a adjacent has not been visited,
+            // then mark it visited and enqueue it in the next queue
+            for (auto i = adj[s].begin(); i != adj[s].end(); ++i)
+            {
+                // Critical section to avoid race condition
+                #pragma omp critical
+                if (!visited[*i])
+                {
+                    visited[*i] = true;
+                    queue[next_turn].push_back(*i);
+                }
+            }
+        }
+
+        turn = (turn + 1) % 2;
+    }
+}
+
 Graph g(10000);
 
 int main()
@@ -150,20 +208,39 @@ int main()
         return 0;
     }
 
+    // Display the number of edges in the graph
     cout << "\nNumber of edges: " << edges << endl;
+
+    // Get the starting vertex
     cout << "\nEnter starting vertex: ";
     cin >> source;
 
-    cout << "Breadth First Search Traversal started...";
+    cout << "Serial Breadth First Search Traversal started...";
 
     clock_t start, end;
+    double execution_time;
+
+    // Serial BFS function with timed execution
     start = clock();
     g.BFS(source);
     end = clock();
 
-    double execution_time = double(end - start) / double(CLOCKS_PER_SEC);
+    execution_time = double(end - start) / double(CLOCKS_PER_SEC);
 
-    cout << "\n\nBFS Completed!\n\nExecution time: " << fixed << execution_time << setprecision(5) << " seconds.\n\n";
+    cout << "\nSerial BFS Completed!\nExecution time: " << fixed << execution_time << setprecision(5) << " seconds.\n\n";
+
+    cout << "Parallel Breadth First Search Traversal started...";
+
+    double stime, ftime;
+
+    // Parallel BFS function with timed execution
+    stime = omp_get_wtime();
+    g.BFS(source);
+    ftime = omp_get_wtime();
+
+    execution_time = ftime - stime;
+
+    cout << "\nParallel BFS Completed!\nExecution time: " << fixed << execution_time << setprecision(5) << " seconds.\n\n";
 
     return 0;
 }
